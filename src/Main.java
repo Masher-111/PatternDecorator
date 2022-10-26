e5da7924abba502fa976ec625472ec5bb93d87f4
Masher-111 <90869108+Masher-111@users.noreply.github.com>
2022-10-26 23:17:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+/*
+implementation of the pattern decorator
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+         Component component = new ConcreteDecorator(new ConcreteComponent());
+         component.operation();
+    }
+}
+/*
+interface
+ */
+interface Component {
+    void operation();
+}
+/*
+class ConcreteComponent implements the interface Component
+ */
+class ConcreteComponent implements Component
+{
+    public void operation(){};
+}
+/*
+intermediate class (abstract)stores a reference
+ to the component interface and constructor
+ */
+abstract class Decorator implements Component
+{
+   Component component;
+    public Decorator(Component component){this.component = component;}
+
+}
+/*
+the class ConcreteDecorator extends the functionality of the interface
+ */
+class ConcreteDecorator extends Decorator
+{
+
+    public ConcreteDecorator(Component component){super(component);}
+    public void operation()
+    {
+
+        component.operation();
+    }
+
+}
